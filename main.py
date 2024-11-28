f5b6deddb0abce730cf1284db1d9f78cdb6fed46
Ahmad Khan <77138985+ahmadtkhan@users.noreply.github.com>
2024-11-27 22:15:44 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -5,18 +5,23 @@ import torch
 torch.cuda.empty_cache()
 
 def main():
-    video_frames = read_video('input_vid/wet_book.mp4')
+    #Read the input mp4 video
+    video_frames = read_video('chunk_002.mp4')
     
+    #The model is trained for detection by the best epoch of the training model
     tracker = Tracker('training/runs/detect/train10/weights/best.pt')
 
+    #The objects are called and put into their lists for each video frame
     tracks = tracker.get_object_tracks(video_frames)
     tracker.detect_shots(tracks)
 
+    #The basketball is interpolated using interpolate_ball function in tracker.py
     tracks["basketball"] = tracker.interpolate_ball(tracks["basketball"])
 
+    #Game like annotations are drawn for each object in each frame and the output video is saved.
     output_video_frames = tracker.draw_annotations(video_frames, tracks)
 
-    save_video(output_video_frames, 'output_vid/test_book_made.avi')
+    save_video(output_video_frames, 'output_vid/infer3_27oct.avi')
 
 if __name__ == '__main__':
     main()
